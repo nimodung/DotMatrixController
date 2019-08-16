f53cf0b296ac581e952a880403cec56e8e4a46bc
nimodung <53554746+nimodung@users.noreply.github.com>
2019-08-16 20:15:46 +0900

    #AVR # Dot Matrix Controller # Atmel Studio

--- DotmatrixController/DotmatrixController/Dot_matrix.h
+++ DotmatrixController/DotmatrixController/Dot_matrix.h
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Dot_matrix.h
  *
  * Created: 2019-04-09 오후 5:07:43
@@ -9,22 +9,15 @@
 #ifndef DOT_MATRIX_H_
 #define DOT_MATRIX_H_
 
-#define dot_matrix_comm_low_DDR DDRB
-#define dot_matrix_comm_high_DDR DDRC
-//공통 단자 : PORTB , PORTC
-#define dot_matrix_data_DDR DDRD
-//데이터 단자 : PORTD
-#define dot_matrix_comm_low_PORT PORTB
-#define dot_matrix_comm_high_PORT PORTC
-#define dot_matrix_data_PORT PORTD
 
-void dotmatrix_init(void);
-void dotmatrix_test(void);
-void dotmatrix_operator(void);
-void dotmatrix_update(char *pattern);
-void dotmatrix_flow(char (*data)[8], char char_len, char interval);
-void dotmatrix_flow_buffer(char (*data)[8], char char_len);
-int Dot_matrix_main(void);
+#define Dot_DDR DDRC
+#define Dot_PORT PORTC
+#define RClk PORTC4
+#define SRClk PORTC5
+#define SER PORTC3
+
+void Dotmatrix_init_shiftR(void);
+void Dotmatrix_shift_out(uint8_t data, uint8_t comm);
 
 
 #endif /* DOT_MATRIX_H_ */
