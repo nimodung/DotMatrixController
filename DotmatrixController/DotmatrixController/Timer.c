2125dd673b6ca3024c745b431fc5673755f85db3
nimodung <53554746+nimodung@users.noreply.github.com>
2019-08-16 20:17:29 +0900

    #AVR # Dot Matrix Controller # Atmel Studio

--- DotmatrixController/DotmatrixController/Timer.c
+++ DotmatrixController/DotmatrixController/Timer.c
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Timer.c
  *
  * Created: 2019-04-10 오전 11:04:02
@@ -9,46 +9,37 @@
 #define F_CPU 16000000UL
 #include <util/delay.h>
 #include <avr/interrupt.h> // interrupt 사용하기 위함
-#include "FND4digit.h"
+
 #include "Timer.h"
+#include "Dot_matrix.h"
 
 
-void Dotmatrix_shift_out(uint8_t data, uint8_t comm);
-//void Dotmatrix_shift_out(char data, char comm);
 extern uint8_t dotmatrix[8];
-extern volatile char dot[8][8];
-//전역 변수는 초기화 안하면 0으로 초기화된다
-volatile int msec = 0, stop_msec = 0, music_i, long_key_msec = 0;				
-volatile char i = 0, j = 0, sec = 0, min = 0, RX_flag;			
-volatile char count;													
+extern char moveMode;
 
-//isr은 가급적 빨리 실행해야 되기 때문에 딜레이 x, 함수호출x ...
-ISR(TIMER0_COMPA_vect)						//interrupt 걸리면 실행하는 내용 //Interrupt Service Routine
+//전역 변수는 초기화 안하면 0으로 초기화된다
+volatile int msec = 0;				
+volatile char i = 0;			
+												
+
+ISR(TIMER0_COMPA_vect)				
 {	
 	
 	
+	msec++;
+	if(msec>=500){
+		msec = 0;
+		PORTB ^= 1 << PORTB5;
+	}
 	
-	
-	//FND를 실제로 출력시키는 
 	i++;
  	
  	if(i >= 8) i = 0;
 	Dotmatrix_shift_out(dotmatrix[i], 0x80 >>i);
-	//Dotmatrix_shift_out(dotmatrix[i], 1 << i);
-
 	
-	//FND_COM_PORT |= FND4digit_digit[i];//< FND_shift_out보다 먼저하면 이전 데이터가 출력돼서 정확한 출력이 나오지 않음
 	
 } 
 
-/* TIMSK0 – Timer/Counter Interrupt Mask Register
-	어떤 interrupt 걸리게 할건지 정하는
-	비교 매치 b, 비교 매치 a, overflow
-*/
-/* TIFR0 – Timer/Counter 0 Interrupt Flag Register
-	interrupt 발생하면 flag 1
-	자동으로 / 상태 register
-*/
 
 void Timer0_init(void)
 {
@@ -57,50 +48,6 @@ void Timer0_init(void)
 	TCCR0B |= (1 << CS00 | 1 << CS01);		//0b00000011;  //64분주 사용(하위 3bit : 011)
 	OCR0A = 249;							//비교할 값 => (16,000,000 / 64) / 1000 = 250 (0 ~ "249")			//1ms 주기
 	TIMSK0 |= 1 << OCIE0A;                  //0b00000010; //어떤 interrupt 걸리게 할건지 정하는	// OC0A 비교 매치 인터럽트 활성화
-	
-	return;
-}
-
-void Timer0_HC_SR04_init(void)
-{
-	TCCR0A |= 1 << WGM01;					//CTC 모드
-	TCCR0B |= (1 << CS02) | (1 << CS00);	//1024분주
-	OCR0A = 255;							//
-	
-	return;
-}
-
-void Timer0_init_CTC_outA(void) //A 출력을 쓰기위한 초기화 함수
-{
-	DDRD |= 1 << PORTD6;
-	TCCR0A |= 1<< WGM01 | 1 <<COM0A0;
-	TCCR0B |= 1 <<  CS00 | 1 <<CS01;
-	
-	OCR0A = 30; //4000Hz
-	//OCR0A = 63; // 2000Hz
-	//OCR0A = 124;							//(250 / 2) - 1 //한 주기를 1msec
-	//OCR0A = 249;							//한 주기 2msec //500Hz
-	//250Hz를 만들고싶다면? => OCR0A를 늘리면 오버플로우(8bit) => 분주를 늘린다
-	//CS0x (분주)랑 OCR값을 이용해서 주기, 주파수 조정
-	TIMSK0 |= 1 << OCIE0A;                  //0b00000010; //어떤 interrupt 걸리게 할건지 정하는	// OC0A 비교 매치 인터럽트 활성화
-	
-	return;
-}
-
-void Timer1_init_CTC_outA(void) //A 출력을 쓰기위한 초기화 함수
-{
-	DDRB |= 1 << PORTB1;
-	TCCR1A |= 1 << COM1A0; //TOGGLE
-	TCCR1B |= 1 << WGM12 | 1 <<  CS11;  //CTC 모드//64분주
-	OCR1A = 0; // 소리 끄기
-	
-	//OCR0A = 30; //4000Hz
-	//OCR0A = 63; // 2000Hz
-	//OCR0A = 124;							//(250 / 2) - 1 //한 주기를 1msec
-	//OCR0A = 249;							//한 주기 2msec //500Hz
-	//250Hz를 만들고싶다면? => OCR0A를 늘리면 오버플로우(8bit) => 분주를 늘린다
-	//CS0x (분주)랑 OCR값을 이용해서 주기, 주파수 조정
-	//TIMSK0 |= 1 << OCIE0A;                  //0b00000010; //어떤 interrupt 걸리게 할건지 정하는	// OC0A 비교 매치 인터럽트 활성화
 	
 	return;
 }
