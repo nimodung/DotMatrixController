3ea2d4370b0601811fb574d0176fbf05682df7d8
nimodung <53554746+nimodung@users.noreply.github.com>
2019-08-16 20:18:11 +0900

    #AVR # Dot Matrix Controller # Atmel Studio

--- DotmatrixController/DotmatrixController/Timer.h
+++ DotmatrixController/DotmatrixController/Timer.h
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Timer.h
  *
  * Created: 2019-04-15 오후 3:36:06
@@ -10,8 +10,5 @@
 #define TIMER_H_
 
 void Timer0_init(void);
-void Timer0_HC_SR04_init(void);
-void Timer1_init_CTC_outA(void);
-void Timer0_init_CTC_outA(void);
 
 #endif /* TIMER_H_ */
